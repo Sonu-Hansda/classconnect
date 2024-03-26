f6a642a8b2e59ec45112394b2ed0feca0c0c5c40
Sonu-Hansda <sonukumarhansda61@gmail.com>
2024-03-26 23:26:53 +0530

    Added firebase to the main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,40 @@
-import 'package:classconnect/screens/landing_page.dart';
+import 'package:classconnect/locator.dart';
+import 'package:classconnect/services/auth.dart';
+import 'package:classconnect/views/home_page.dart';
+import 'package:classconnect/views/landing_page.dart';
+import 'package:classconnect/views/sign_in/cubit/sign_in_cubit.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  setupLocator();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  MyApp({super.key});
+  final AuthService authService = locator<AuthService>();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Class Connect',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        textTheme: GoogleFonts.robotoTextTheme(Theme.of(context).textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<SignInCubit>(
+            create: (context) => SignInCubit(authService)),
+      ],
+      child: MaterialApp(
+        title: 'Class Connect',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          textTheme: GoogleFonts.robotoTextTheme(Theme.of(context).textTheme),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: authService.isLoggedIn() ? const HomePage() : const LandingPage(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: const LandingPage(),
     );
   }
 }
